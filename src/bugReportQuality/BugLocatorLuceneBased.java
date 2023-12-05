bbfb11afa31d2d7e8c3ab78a1b631ced704baafe
Mukta <Mukta@DESKTOP-PM4B8HQ.home>
2023-12-05 12:23:13 -0400

    Required code and libraries for lucene-based bug localization are added.

---
+++ src/bugReportQuality/BugLocatorLuceneBased.java
@@ -0,0 +1,199 @@
+package bugReportQuality;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+import utility.ContentLoader;
+import utility.ContentWriter;
+import utility.MiscUtility;
+
+public class BugLocatorLuceneBased {
+
+	
+	static String indexDir;
+	String bugReportFolder;
+	int no_of_source_code;
+
+	public BugLocatorLuceneBased(String indexDir, String bugReportFolder) {
+		this.indexDir = indexDir;
+		this.bugReportFolder = bugReportFolder;
+	}
+	public BugLocatorLuceneBased(String indexDir, String bugReportFolder, int no_of_source_code) {
+		this.indexDir = indexDir;
+		this.bugReportFolder = bugReportFolder;
+		this.no_of_source_code=no_of_source_code;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String corpusName="imgui";
+		String base="C:\\Users\\mukta\\OneDrive\\Documents\\PhD\\VisualBugProject\\Experiment\\DataSet\\Project1\\Corpus";
+		String indexFolder=base+"\\Index_"+corpusName+"\\";
+		//String bugReportFolder = "C:\\Users\\mukta\\OneDrive\\Documents\\PhD\\VisualBugProject\\Experiment\\DataSet\\Corpus\\IssueReports";
+		//With image text
+		//String bugReportFolder = "C:\\Users\\mukta\\OneDrive\\Documents\\PhD\\VisualBugProject\\Experiment\\DataSet\\Corpus\\IssueReportsWithImageText";
+		//Testing
+		String bugReportFolder = "C:\\Users\\mukta\\OneDrive\\Documents\\PhD\\VisualBugProject\\Experiment\\DataSet\\Project1\\Corpus\\IssueReporstForImageAnalysis\\WithImageTesting\\";
+	    //String subproject="CAMEL";
+        // String version="1_6_0";  
+		//String indexDir="C:/Users/Shamima/Documents/"+"E\\PhD\\Repo\\"+project+"\\"+subproject+"\\"+version+"\\data\\Index"+project+subproject+version+"\\";
+		//String bugReportFolder = "C:\\Users\\Mukta\\Workspace-2018\\QueryReformulation\\data\\testsetForBL\\test10";
+		//String bugReportFolder = "C:/Users/Shamima/Documents/"+"E\\PhD\\Repo\\"+project+"\\"+subproject+"\\"+version+"\\data\\testsetForVSM";
+		BugLocatorLuceneBased obj=new BugLocatorLuceneBased(indexFolder,bugReportFolder,418);
+		HashMap<Integer, HashMap<String, Double>>finalResultHM=obj.getLuceneBasedScore();
+		//System.out.println(finalResultHM);
+		//obj.writeFinalResult(finalResultHM, "C:/Users/Shamima/Documents/"+"E\\PhD\\Repo\\"+project+"\\"+subproject+"\\"+version+"\\DataForFeatureExtraction\\RankInfo\\VSM100.txt");
+		ContentWriter.writeContentFinalResult(base+"\\FinalResultNoImageTestingDec05.txt", finalResultHM);
+	}
+    public HashMap<Integer, HashMap<String, Double>> getLuceneBasedScore()
+    {
+        HashMap<Integer, HashMap<String, Double>>finalResult=new HashMap<>();
+        HashMap<String, ArrayList<String>> docMap=getBugReportContentMap(this.bugReportFolder);
+        
+        for(String bugID: docMap.keySet())
+        {
+            int queryID=Integer.valueOf(bugID.substring(0,bugID.length()-4));
+            ArrayList<String> content=docMap.get(bugID);
+            System.out.println(bugID);
+            String queryContent=MiscUtility.list2Str(content);
+            //System.out.println(queryContent);
+            HashMap<String, Double> resultPerQuery=searchIndex(queryContent, 1.0);
+            //System.out.println(resultPerQuery.size());
+            //MiscUtility.showResult(90, resultPerQuery);
+            finalResult.put(queryID, resultPerQuery);
+        }
+        
+        
+        return finalResult;
+    }
+	public HashMap<Integer, HashMap<String, Double>> getLuceneBasedScore(Double ALPHA)
+	{
+		HashMap<Integer, HashMap<String, Double>>finalResult=new HashMap<>();
+		HashMap<String, ArrayList<String>> docMap=getBugReportContentMap(this.bugReportFolder);
+		
+		for(String bugID: docMap.keySet())
+		{
+			int queryID=Integer.valueOf(bugID.substring(0,bugID.length()-4));
+			ArrayList<String> content=docMap.get(bugID);
+			//System.out.println(bugID);
+			String queryContent=MiscUtility.list2Str(content);
+			//System.out.println(queryContent);
+			HashMap<String, Double> resultPerQuery=searchIndex(queryContent, ALPHA);
+			//System.out.println(resultPerQuery.size());
+			//MiscUtility.showResult(90, resultPerQuery);
+			finalResult.put(queryID, resultPerQuery);
+		}
+		
+		
+		return finalResult;
+	}
+	
+	protected static void writeFinalResult(HashMap<Integer, HashMap<String, Double>>finalResultHM, String outFile)
+	{
+		
+		HashMap<String, Double> hm=new HashMap<>();
+		ArrayList<String> list=new ArrayList<>();
+ 		int count=0;
+		for(int key:finalResultHM.keySet())
+		{
+			
+			hm=finalResultHM.get(key);
+			count=0;
+			for(String sid:hm.keySet())
+			{
+				count++;
+				if(count>10)break; 
+				list.add(key+","+sid+","+hm.get(sid));
+			}
+		}
+		
+		ContentWriter.writeContent(outFile, list);
+		
+	}
+	protected static HashMap<String, ArrayList<String>> getBugReportContentMap(
+			String bugReportFolder) {
+		File[] files = new File(bugReportFolder).listFiles();
+		HashMap<String, ArrayList<String>> docMap = new HashMap<>();
+		for (File file : files) {
+			ArrayList<String> docTokens = ContentLoader.getDocTokensAll(file
+					.getAbsolutePath());
+
+			docMap.put(file.getName(), docTokens);
+		}
+		return docMap;
+	}
+	
+	public HashMap<String, Double> searchIndex(String searchString, double ALPHA) {
+
+		HashMap<String, Double> resultMap=new HashMap<>();
+
+		try {
+			Analyzer analyzer = new StandardAnalyzer();
+			FSDirectory dir = FSDirectory.open(new File(this.indexDir).toPath());
+            System.out.println(searchString+" "+indexDir.length());
+			IndexReader indexReader;
+			indexReader=DirectoryReader.open(dir);
+			IndexSearcher searcher = new IndexSearcher(indexReader);
+
+			int hitsPerPage = this.no_of_source_code;
+			System.out.println(hitsPerPage);
+			//int hitsPerPage = 100;
+			QueryParser qp = new QueryParser("contents",
+					analyzer);
+			Query query = qp.parse(searchString); // parse the query and construct the Query object
+			TopDocs docs = searcher.search(query, hitsPerPage);
+			ScoreDoc[] hits = docs.scoreDocs;// run the query
+			//for(int i=0;i<hits.length;++i) {
+			for(int i=0;i<10;++i) {
+			{
+				ScoreDoc item = hits[i];
+	        	Document doc = searcher.doc(item.doc);
+	        	double score=item.score;
+	        	System.out.println((i + 1) + ". " + doc.get("path") + "\t"+score);
+	        	resultMap.put(doc.get("path"), score);
+			}
+		
+			
+           }
+		indexReader.close(); 
+		} catch (Exception e) {
+			e.printStackTrace();
+	}
+		
+		HashMap<String, Double> resultMapNormalized=normalizeMe(resultMap, ALPHA);
+		HashMap<String, Double> resultSorted=MiscUtility.sortByValues(resultMapNormalized);
+		return resultSorted;
+}
+	
+	
+	protected static HashMap<String, Double> normalizeMe(
+			HashMap<String, Double> tempMap, double ALPHA) {
+		double max = 0;
+		for (String key : tempMap.keySet()) {
+			double score = tempMap.get(key);
+			if (score > max) {
+				max = score;
+			}
+		}
+		for (String key : tempMap.keySet()) {
+			if (max > 0) {
+				double oldScore = tempMap.get(key);
+				tempMap.put(key, (oldScore / max)*ALPHA);
+			}
+		}
+		return tempMap;
+	}
+}
---
+++ src/utility/ContentWriter.java
@@ -0,0 +1,139 @@
+package utility;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ContentWriter {
+	public static boolean writeContent(String outFile, ArrayList<String> items) {
+		// writing content to output
+		boolean written = false;
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile));
+			for (String item : items) {
+				fwriter.write(item+"\n");
+			}
+			fwriter.close();
+			written = true;
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return written;
+	}
+	
+	public static boolean writeFormulatedQueriesContent(String outFile, ArrayList<String> items) {
+		// writing content to output
+		boolean written = false;
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile));
+			for (String item : items) {
+				fwriter.write(item+"\r\n");
+			}
+			fwriter.close();
+			written = true;
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return written;
+	}
+
+	public static boolean appendContent(String outFile, ArrayList<String> items) {
+		// writing content to output
+		boolean written = false;
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile), true);
+			for (String item : items) {
+				fwriter.write(item + "\n");
+			}
+			fwriter.close();
+			written = true;
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return written;
+	}
+
+	public static boolean writeContentInt(String outFile,
+			ArrayList<Integer> items) {
+		// writing content to output
+		boolean written = false;
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile));
+			for (Integer item : items) {
+				fwriter.write(item + "\n");
+			}
+			fwriter.close();
+			written = true;
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return written;
+	}
+
+	public static boolean appendContentInt(String outFile,
+			ArrayList<Integer> items) {
+		// writing content to output
+		boolean written = false;
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile), true);
+			for (Integer item : items) {
+				fwriter.write(item + "\n");
+			}
+			fwriter.close();
+			written = true;
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return written;
+	}
+
+	public static void writeContent(String outFile, String content) {
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile));
+			fwriter.write(content);
+			fwriter.close();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	public static void appendContent(String outFile, String content) {
+		try {
+			FileWriter fwriter = new FileWriter(new File(outFile), true);
+			fwriter.write(content+"\n");
+			fwriter.close();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	 public static boolean writeContentFinalResult(String outFile, HashMap<Integer, HashMap<String, Double>> resultContainer) {
+        // TODO Auto-generated method stub
+        //System.out.println(resultContainer);
+        boolean written = false;
+        try {
+            FileWriter fwriter = new FileWriter(new File(outFile), true);
+            for (Integer key:resultContainer.keySet()) {
+                fwriter.write(key+ "\n");
+                HashMap<String, Double> queryContent=resultContainer.get(key);
+                for(String key2:queryContent.keySet())
+                {
+                    fwriter.write(key2+"\n");
+                }
+            }
+            fwriter.close();
+            written = true;
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        return written;
+    }
+
+}
